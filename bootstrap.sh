ba25757d2e4469d971e6b4510a6b27b423025f1d
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-04-01 14:46:59 +0100

    Added jenkins configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,6 +7,9 @@ source /etc/profile
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install jenkins -y
+rm -rf /var/lib/jenkins
+git clone https://github.com/bharatmicrosystems/filestore.git
+cd filestore && cp -a jenkins /var/lib/jenkins
 systemctl enable jenkins
 systemctl start jenkins
 firewall-cmd --zone=public --permanent --add-port=8080/tcp
