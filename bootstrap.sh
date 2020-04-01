2c835662379bdca0fe1f7e118f045fbeece8908c
Agarwal <gaurav.a.agarwal@capgemini.com>
2020-04-01 12:15:50 +0100

    Updated wget

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 yum install epel-release -y
 yum install java-1.8.0-openjdk.x86_64 -y
+yum install wget -y
 echo 'export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk' | sudo tee -a /etc/profile
 echo 'export JRE_HOME=/usr/lib/jvm/jre' | sudo tee -a /etc/profile
 source /etc/profile
@@ -9,4 +10,4 @@ yum install jenkins -y
 systemctl enable jenkins
 systemctl start jenkins
 firewall-cmd --zone=public --permanent --add-port=8080/tcp
-firewall-cmd --reload 
+firewall-cmd --reload
